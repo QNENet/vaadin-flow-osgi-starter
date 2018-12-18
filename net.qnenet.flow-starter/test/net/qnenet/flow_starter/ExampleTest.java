2428c0e4e08edab1524d966ce57458df70351207
paulf <paulf@qnenet.com>
2018-12-18 19:51:49 +1100

    First remote commit

---
+++ net.qnenet.flow-starter/test/net/qnenet/flow_starter/ExampleTest.java
@@ -0,0 +1,14 @@
+package net.qnenet.flow_starter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
